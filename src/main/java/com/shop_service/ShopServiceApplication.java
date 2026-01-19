72027371dfe7d037cc6b0040bb63d7cd74fd2021
33101 <3310132142@qq.com>
2026-01-20 00:58:16 +0800

    修复必要注解缺失

--- src/main/java/com/shop_service/ShopServiceApplication.java
+++ src/main/java/com/shop_service/ShopServiceApplication.java
@@ -1,8 +1,14 @@
 package com.shop_service;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAsync
+@EnableScheduling
+@MapperScan("com.shop_service.mapper")
 @SpringBootApplication
 public class ShopServiceApplication {
 
