f15c3ef9e310f8d607724952916df180e0bbb65a
33101 <3310132142@qq.com>
2026-02-02 16:32:22 +0800

    优化注释

--- src/main/java/com/shop_service/model/response/AdminStatsVo.java
+++ src/main/java/com/shop_service/model/response/AdminStatsVo.java
@@ -48,42 +48,42 @@ public class AdminStatsVo {
     private Long destroyCardSize;
 
     /**
-     * 消费金额
+     * 卡片消费金额
      */
     private BigDecimal cardConsumptionAmount;
 
     /**
-     * 转入金额
+     * 卡片转入金额
      */
     private BigDecimal cardTransferInAmount;
 
     /**
-     * 转出金额
+     * 卡片转出金额
      */
     private BigDecimal cardTransferOutAmount;
 
     /**
-     * 冻结金额
+     * 卡片冻结金额
      */
     private BigDecimal cardFrozenAmount;
 
     /**
-     * 解冻金额
+     * 卡片解冻金额
      */
     private BigDecimal cardUnfrozenAmount;
 
     /**
-     * 撤销金额
+     * 卡片撤销金额
      */
     private BigDecimal cardReversalAmount;
 
     /**
-     * 退款金额
+     * 卡片退款金额
      */
     private BigDecimal cardCreditAmount;
 
     /**
-     * 授权金额
+     * 卡片授权金额
      */
     private BigDecimal cardFeeConsumptionAmount;
 }
